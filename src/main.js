2b81dee414e887baf7341213156ee72fcab80956
nonso01 <chukwunonsomartin01@gmail.com>
2022-11-06 10:24:21 +0100

    now am working

---
+++ src/main.js
@@ -0,0 +1,9 @@
+"use strict"
+
+const csl=(x)=>console.log(x)
+const dc=(x)=>document.createElement(x)
+const dq=(x)=>document.querySelector(x)
+const dqA=(x)=> document.querySelectorAll(x)
+
+const body= dq('body')
+const html= dq("html")
